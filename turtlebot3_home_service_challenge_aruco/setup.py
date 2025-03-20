29a1a5d765170eddc377aa24a98217b4cee6652a
chan-1207 <dddoggi1207@gmail.com>
2025-03-20 12:06:04 +0900

    add resource file

--- turtlebot3_home_service_challenge_aruco/setup.py
+++ turtlebot3_home_service_challenge_aruco/setup.py
@@ -6,7 +6,7 @@ package_name = 'turtlebot3_home_service_challenge_aruco'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='2.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
