a990b88ae2c0d77f85bf8728a850bac917c0f348
chan-1207 <dddoggi1207@gmail.com>
2025-03-20 11:04:29 +0900

    Fix setup.py

--- turtlebot3_home_service_challenge_core/setup.py
+++ turtlebot3_home_service_challenge_core/setup.py
@@ -9,6 +9,7 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
